ed020ad73e3cea624bdde4a5acfac8cb3230e114
Gleb Minevich <realgodd@gmail.com>
2016-09-21 19:55:09 +0300

    implemented resources and properties

--- js/game.js
+++ js/game.js
@@ -46,6 +46,84 @@ var Resource = function(settings){
 		return count;
 	};
 };
+var Action = function(settings) {
+	var me = this;
+	this.name = settings.name;
+	this.actions = settings.actions;
+};
+var Property = function(settings) {
+	var me = this;
+	var tier = 0;
+	this.name = settings.name;
+	this.value = function(){
+		return settings.values[tier];
+	};
+	this.upgrade = function() {
+		tier++;
+	};
+	this.tick = function() {
+		var chance = Math.random(1);
+		if(chance < settings.chance) {
+			return settings.outcome(tier);
+		} else {
+			//console.log(settings.name, chance + " > " + settings.chance);
+		}
+		return {type: "stall"};
+	};
+};
+var leaderProperty = new Property({
+	name: "Leader",
+	values: ["Primate", "Chief", "King", "Emperor", "President"],
+	chance: 0.1,
+	outcome: function(tier) {
+		if(tier == 0) {
+			return [
+				{
+					type: "resource",
+					object: new Resource({
+						name: "Food",
+						increment: 0.1
+					})
+				},
+				{
+					type: "upgrade"
+				}
+			];
+		}
+	}
+});
+var testProperty = new Property({
+	name: "Society",
+	values: ["Tribe", "Tribe v.2"],
+	chance: 0.4,
+	outcome: function(tier) {
+		if(tier == 0) {
+			return [
+				{
+					type: "action",
+					object: new Action({
+						name: "Recruit a leader",
+						actions: [
+							{
+								name: "Recruit",
+								action: function() {
+									return {
+										type: "property",
+										change: "add",
+										object: leaderProperty
+									}
+								}
+							}
+						]
+					})
+				},
+				{
+					type: "upgrade"
+				}
+			];
+		}
+	}
+});
 var PoliticalSystem = function() {
 	var me = this;
 	var resources = [];
@@ -63,24 +141,46 @@ var PoliticalSystem = function() {
 			resources.push(newResource);
 		}
 	};
+	this.addProperty = function(newProperty) {
+		var found = false;
+		properties.forEach(function(property) {
+			if(property.name == newProperty.name) {
+				found = true;
+			}
+		});
+		if(!found) {
+			properties.push(newProperty);
+		}
+	};
 	this.tick = function() {
 		resources.forEach(function(resource){
 			resource.tick(properties);
 		});
 		properties.forEach(function(property){
-			var outcome = property.tick();
-			if(outcome.type == "action") {
-				availableActions.push(outcome.object);
-			} else if(outcome.type == "resource") {
-				me.addResource(outcome.object);
+			var outcomes = property.tick();
+			for(var i in outcomes) {
+				var outcome = outcomes[i];
+				if(outcome.type == "action") {
+					availableActions.push(outcome.object);
+				} else if(outcome.type == "resource") {
+					me.addResource(outcome.object);
+				} else if(outcome.type == "upgrade") {
+					property.upgrade();
+				}
 			}
 		});
 	};
-	this.report = function(propertiesRoot, resourcesRoot, decisionsRoot) {
-		var formatter = new ValueFormatter();
-		while(resourcesRoot.firstChild) {
-			resourcesRoot.removeChild(resourcesRoot.firstChild);
+	var cleanContainer = function(container) {
+		while(!!container && container.firstChild) {
+			container.removeChild(container.firstChild);
 		}
+	}
+	this.report = function(propertiesRoot, resourcesRoot, decisionsRoot) {
+		//TODO: report function should not redraw every element on screen
+		var formatter = new ValueFormatter();
+		cleanContainer(resourcesRoot);
+		cleanContainer(propertiesRoot);
+		cleanContainer(decisionsRoot);
 		resources.forEach(function(resource){
 			var el = _("div", function(item){
 				item.className = "item";
@@ -91,9 +191,51 @@ var PoliticalSystem = function() {
 				item.appendChild(_("div", function(value){
 					value.className = "value";
 					value.innerHTML = formatter.format(resource.count());
-				}))
+				}));
 			});
 			resourcesRoot.appendChild(el);
+		});
+		properties.forEach(function(property){
+			var el = _("div", function(item){
+				item.className = "item";
+				item.appendChild(_("div", function(name){
+					name.className = "name";
+					name.innerHTML = property.name;
+				}));
+				item.appendChild(_("div", function(value){
+					value.className = "value";
+					value.innerHTML = property.value();
+				}));
+			});
+			propertiesRoot.appendChild(el);
+		});
+		availableActions.forEach(function(action){
+			var el = _("div", function(item){
+				item.className = "item";
+				item.appendChild(_("div", function(name){
+					name.className = "name";
+					name.innerHTML = action.name;
+				}));
+				action.actions.forEach(function(actualAction) {
+					item.appendChild(_("div", function(button){
+						button.className = "button action";
+						button.innerHTML = actualAction.name;
+						button.onclick = function() {
+							var removedActions = availableActions.splice(availableActions.indexOf(action), 1);
+							var removedAction = removedActions[0];
+							if(removedAction) {
+								var outcome = actualAction.action();
+								if(outcome.type == "property") {
+									if(outcome.change == "add") {
+										me.addProperty(outcome.object);
+									}
+								}
+							}
+						};
+					}));
+				});
+			});
+			decisionsRoot.appendChild(el);
 		});
 	};
 };
@@ -106,7 +248,7 @@ window.run = function(){
 	var progress = 0;
 	var minProgress = 0;
 	var maxProgress = 100;
-	var tickRate = 200;
+	var tickRate = 15;
 	var descriptions = ["Dummy progress..", "Inspecting banks..", "Collecting taxes..", "Daydreaming..", "Arguing on TV..", "Laughing at people..", "Inventing lies.."];
 	var r = function(array) {
 		var i = (Math.random() * array.length);
@@ -117,20 +259,22 @@ window.run = function(){
 
 	var testResource = new Resource({
 		name: "Gold",
-		increment: 0.01s
+		increment: 0.0001
 	});
 	system.addResource(testResource);
+	system.addProperty(testProperty);
 
-	var performActions = function() {
+	var perform = function() {
 		description.innerHTML = r(descriptions);
 		system.tick();
+		//TODO: move to incProgress and rewrite the report function
 		system.report(propertiesRoot, resourcesRoot, decisionsRoot);
 	};
 	var incProgress = function(){
 		progress++;
 		if(progress > maxProgress) {
 			progress = minProgress;
-			performActions();
+			perform();
 		}
 	};
 	var setProgress = function(){
@@ -141,5 +285,6 @@ window.run = function(){
 		setProgress();
 		setTimeout(tick, tickRate);
 	};
+	system.report(propertiesRoot, resourcesRoot, decisionsRoot);
 	tick();
 }
